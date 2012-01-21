ce22baa465f505cf094bf52503fe6e2677a3126b
Samuel Huang <schuang@ucdavis.edu>
2012-01-21 12:10:38 -0800

    Refactored with eclipse IDE!

--- Entry.java
+++ Entry.java
@@ -1,54 +1,61 @@
-import java.util.*;
+//import java.util.*;
 
 class Entry {
-    private String name;        // name of symbol.
-    private int lineNumber;     // line on which declared
-    private TK varOrConst;      // variable or const?
-    private boolean isIV ;      // is presently index variable?
-    private boolean isArray;	// is the variable an array?
+	private String name; // name of symbol.
+	private int lineNumber; // line on which declared
+	private TK varOrConst; // variable or const?
+	private boolean isIV; // is presently index variable?
+	private boolean isArray; // is the variable an array?
 
-    public Entry(String name, int lineNumber, TK varOrConst) {
-        this.name = name;
-        this.lineNumber = lineNumber;
-        this.varOrConst = varOrConst;
-        this.isIV = false;
-	this.isArray = false;
-    }
-    String getName() {
-        return name;
-    }
-    int getLineNumber() {
-        return lineNumber;
-    }
-    boolean isVar() {
-        return (varOrConst == TK.VAR);
-    }
-    boolean isConst() {
-        return (varOrConst == TK.CONST);
-    }
-    boolean getIsIV() {
-        return isIV;
-    }
-    void setIsIV(boolean is) {
-        isIV = is;
-    }
-    void setIsArray(boolean is) { // part 13
-	isArray = is;      
-    }
-    boolean getIsArray() {	// part 13
-	return isArray;      
-    }
-    
-    public String whatAreYou() {
-// 	if (isArray) {
-// 	    return "array";
-// 	}
-        return  getIsArray()?"array"
-	       :isVar()?"variable"
-               :isConst()?"constant"
-               :"OOPS Entry whatAreYou()";
-    }
-    public String toString() {
-        return name + "  declared on line " + lineNumber + " as "+ varOrConst;
-    }
+	public Entry(String name, int lineNumber, TK varOrConst) {
+		this.name = name;
+		this.lineNumber = lineNumber;
+		this.varOrConst = varOrConst;
+		this.isIV = false;
+		this.isArray = false;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	int getLineNumber() {
+		return lineNumber;
+	}
+
+	boolean isVar() {
+		return (varOrConst == TK.VAR);
+	}
+
+	boolean isConst() {
+		return (varOrConst == TK.CONST);
+	}
+
+	boolean getIsIV() {
+		return isIV;
+	}
+
+	void setIsIV(boolean is) {
+		isIV = is;
+	}
+
+	void setIsArray(boolean is) { // part 13
+		isArray = is;
+	}
+
+	boolean getIsArray() { // part 13
+		return isArray;
+	}
+
+	public String whatAreYou() {
+		// if (isArray) {
+		// return "array";
+		// }
+		return getIsArray() ? "array" : isVar() ? "variable"
+				: isConst() ? "constant" : "OOPS Entry whatAreYou()";
+	}
+
+	public String toString() {
+		return name + "  declared on line " + lineNumber + " as " + varOrConst;
+	}
 }
--- Scan.java
+++ Scan.java
@@ -8,307 +8,306 @@ import java.io.*;
 
 public class Scan {
 
-    // default max length of identifiers; truncate extra.
-    public static final int MAXLEN_ID = 40;
+	// default max length of identifiers; truncate extra.
+	public static final int MAXLEN_ID = 40;
 
-    public Scan(String args[]) { // the constructor
-        // open an input file if one specified on command line.
-        // o.w., use standard input.
-        try {
-            if (args.length == 0) {
-                InputStreamReader isr = new InputStreamReader(System.in);
-                br = new BufferedReader(isr);
-            }
-            else if (args.length > 1) {
-                System.err.println("too many command line arguments("+
-                                   args.length+"); want 0 or 1" );
-                System.exit(1);
-            }
-            else {
-                FileReader fr = new FileReader(args[0]);
-                br = new BufferedReader(fr);
-            }
-        }
-        catch (Exception oops) {
-            System.err.println("Exception opening file "+args[0]+"\n");
-            oops.printStackTrace();
-        }
+	public Scan(String args[]) { // the constructor
+		// open an input file if one specified on command line.
+		// o.w., use standard input.
+		try {
+			if (args.length == 0) {
+				InputStreamReader isr = new InputStreamReader(System.in);
+				br = new BufferedReader(isr);
+			} else if (args.length > 1) {
+				System.err.println("too many command line arguments("
+						+ args.length + "); want 0 or 1");
+				System.exit(1);
+			} else {
+				FileReader fr = new FileReader(args[0]);
+				br = new BufferedReader(fr);
+			}
+		} catch (Exception oops) {
+			System.err.println("Exception opening file " + args[0] + "\n");
+			oops.printStackTrace();
+		}
 
-        // initially, we pretend that we just read a newline.
-        // hence linenumber is initialized to 0.
-        c = '\n';
-        linenumber = 0;
-        putback = true;
-        got_eof = false;
-    }
+		// initially, we pretend that we just read a newline.
+		// hence linenumber is initialized to 0.
+		c = '\n';
+		linenumber = 0;
+		putback = true;
+		got_eof = false;
+	}
 
-    // internal state of scanner
-    private BufferedReader br;  // input stream
-    private String token;    // the token as a string
-    private TK tkrep;        // the kind of token
-    private int linenumber;  // line number
+	// internal state of scanner
+	private BufferedReader br; // input stream
+	// private String token; // the token as a string
+	// private TK tkrep; // the kind of token
+	private int linenumber; // line number
 
-    private boolean got_eof; // true iff have seen EOF
-    private boolean putback; // true iff put a char back
-    private int c;           // current or putback char
-                             // (int rather than char to handle EOF)
-    private static final int EOF = -1;
+	private boolean got_eof; // true iff have seen EOF
+	private boolean putback; // true iff put a char back
+	private int c; // current or putback char
+					// (int rather than char to handle EOF)
+	private static final int EOF = -1;
 
-    // call to advance token stream.
-    // acts as a generator (iterator) over input.
-    // returns Token
-    public Token scan() {
-        if( got_eof ) {
-            System.err.println("scan: oops -- called after eof.");
-            return new Token(TK.ERROR, "called after eof", linenumber);
-        }
+	// call to advance token stream.
+	// acts as a generator (iterator) over input.
+	// returns Token
+	public Token scan() {
+		if (got_eof) {
+			System.err.println("scan: oops -- called after eof.");
+			return new Token(TK.ERROR, "called after eof", linenumber);
+		}
 
-        while(true) {
-// QUESTION 1: What is purpose of putback?
+		while (true) {
+			// QUESTION 1: What is purpose of putback?
 
-// QUESTION 2: What are the values of putback and c just before the identifier
-//             'hello' is returned from the input 'hello*45'?
+			// QUESTION 2: What are the values of putback and c just before the
+			// identifier
+			// 'hello' is returned from the input 'hello*45'?
 
-            if( putback) {
-                putback = false;  // use last char
-            }
-            else {
-                c = getchar();    // get new char
-            }
-            
-            if ( myisalpha((char) c) ) {
-                /* identifier. */
-                String id = buildID();
-                return new Token(keywordLookup(id), id, linenumber);
-            }
-            else if ( myisdigit((char) c) ) {
-                /* number. */
-                return new Token(TK.NUM, buildNUM(), linenumber);
-            }
-             else if (c == '"') {
+			if (putback) {
+				putback = false; // use last char
+			} else {
+				c = getchar(); // get new char
+			}
+
+			if (myisalpha((char) c)) {
+				/* identifier. */
+				String id = buildID();
+				return new Token(keywordLookup(id), id, linenumber);
+			} else if (myisdigit((char) c)) {
+				/* number. */
+				return new Token(TK.NUM, buildNUM(), linenumber);
+			} else if (c == '"') {
 				/* string. */
 				return new Token(TK.DQUOTE, buildSTR(), linenumber);
-			}
-            else {
-                switch( c ) {
-                    case ',':
-                        return ccase1(',',TK.COMMA);
-// 		    case '"':
-//                         return ccase1('"',TK.DQUOTE);
-                    case '(':
-                        return ccase1('(',TK.LPAREN);
-                    case ')':
-                        return ccase1(')',TK.RPAREN);
+			} else {
+				switch (c) {
+				case ',':
+					return ccase1(',', TK.COMMA);
+					// case '"':
+					// return ccase1('"',TK.DQUOTE);
+				case '(':
+					return ccase1('(', TK.LPAREN);
+				case ')':
+					return ccase1(')', TK.RPAREN);
 					// for part 13
-                    case '[':
-                        return ccase1('[',TK.LBRACKET);
-                    case ']':
-                        return ccase1(']',TK.RBRACKET);
+				case '[':
+					return ccase1('[', TK.LBRACKET);
+				case ']':
+					return ccase1(']', TK.RBRACKET);
 					// end for part 13
-                    case '*':
-                        return ccase1('*',TK.TIMES);
-                    case '=':
-                        return ccase1('=',TK.EQ);
-                    case '+':    
-                        return ccase1('+',TK.PLUS);
-                    case '-':
-                        return ccase1('-', TK.MINUS);
+				case '*':
+					return ccase1('*', TK.TIMES);
+				case '=':
+					return ccase1('=', TK.EQ);
+				case '+':
+					return ccase1('+', TK.PLUS);
+				case '-':
+					return ccase1('-', TK.MINUS);
 
-// QUESTION 3:  What does the following case and the code in it do?
-                    case '/':
-                        return ccase1or2('/','=',TK.DIVIDE,TK.NE);
+					// QUESTION 3: What does the following case and the code in
+					// it do?
+				case '/':
+					return ccase1or2('/', '=', TK.DIVIDE, TK.NE);
 
-                    case '<':
-                        return ccase1or2('<','=',TK.LT,TK.LE);
-                    case '>':
-                        return ccase1or2('>','=',TK.GT,TK.GE);
+				case '<':
+					return ccase1or2('<', '=', TK.LT, TK.LE);
+				case '>':
+					return ccase1or2('>', '=', TK.GT, TK.GE);
 
-                    case ':':
-                        return ccase1or2(':','=',TK.COLON,TK.ASSIGN);// part 13
+				case ':':
+					return ccase1or2(':', '=', TK.COLON, TK.ASSIGN);// part 13
 
-                    case EOF:
-                        got_eof = true;
-                        return new Token(TK.EOF,
-                                         new String("*EOF*"),
-                                         linenumber);
-                    case '\n':
-                        linenumber++;
-                        break;
-                    case ' ':
-                    case '\t':
-                    case '\r': // for Windows (lines end in \r\n)
-                        break; // whitespace is easy to ignore
-                    case '#': // gobble comments
-                        do {
-                            c = getchar();
-                        } while( c != '\n' && c != EOF );
-                        putback = true;
-                        break;
-                    default:
-                        System.err.println(
-                                           "scan: line "+linenumber+
-                                           " bad char (ASCII " + c
-                                           + ")");
-                        break;
-                }
-            }
-        }
-    }
+				case EOF:
+					got_eof = true;
+					return new Token(TK.EOF, new String("*EOF*"), linenumber);
+				case '\n':
+					linenumber++;
+					break;
+				case ' ':
+				case '\t':
+				case '\r': // for Windows (lines end in \r\n)
+					break; // whitespace is easy to ignore
+				case '#': // gobble comments
+					do {
+						c = getchar();
+					} while (c != '\n' && c != EOF);
+					putback = true;
+					break;
+				default:
+					System.err.println("scan: line " + linenumber
+							+ " bad char (ASCII " + c + ")");
+					break;
+				}
+			}
+		}
+	}
 
+	private int getchar() {
+		int c = EOF;
+		try {
+			c = br.read();
+		} catch (java.io.IOException e) {
+			System.err.println("oops ");
+			e.printStackTrace();
+		}
+		return c;
+	}
 
-    private int getchar() {
-        int c = EOF;
-        try {
-            c = br.read();
-        } catch (java.io.IOException e) {
-            System.err.println("oops ");
-            e.printStackTrace();
-        }
-        return c;
-    }
+	private Token ccase1(char c, TK r) {
+		return new Token(r, new String(String.valueOf(c)), linenumber);
+	}
 
-    private Token ccase1(char c, TK r) {
-        return new Token(r, new String(String.valueOf(c)), linenumber);
-    }
-
-    private Token ccase1or2(char c1, char c2, TK r1, TK r2) {
-		// if the next char is c2, then TK type is r2, 
+	private Token ccase1or2(char c1, char c2, TK r1, TK r2) {
+		// if the next char is c2, then TK type is r2,
 		// else putback the char and the current one is of type r1.
-        c = getchar();
-        if (c == c2) {
-            return new Token(
-                    r2,
-                    new String(String.valueOf(c1)+String.valueOf(c2)),
-                    linenumber);
-        }
-        else {
-            putback = true;
-            return new Token(r1, new String(String.valueOf(c1)), linenumber);
-        }
-    }
-
-    private Token ccase2(char c1, char c2, TK r1, TK r2) {
-		// the next char must be c2 otherwise it's an error.
-        c = getchar();
-        if (c == c2) {
-            return new Token(
-                     r2, String.valueOf(c1)+String.valueOf(c2),
-                     linenumber);
-        }
-	else if( myisdigit( (char) c ) ){
-	    String number = buildNUM();
-	    putback = false;
-	    return new Token( r1, number, linenumber );
+		c = getchar();
+		if (c == c2) {
+			return new Token(r2, new String(String.valueOf(c1)
+					+ String.valueOf(c2)), linenumber);
+		} else {
+			putback = true;
+			return new Token(r1, new String(String.valueOf(c1)), linenumber);
+		}
 	}
-	else {
-            System.err.println("scan: got got " + c1 +
-                               " missing " + c2 +
-                               " (got ASCII " + c + ")");
-            return new Token(TK.ERROR, "bad ccase2", linenumber);
+
+	/*
+	 * private Token ccase2(char c1, char c2, TK r1, TK r2) { // the next char
+	 * must be c2 otherwise it's an error. c = getchar(); if (c == c2) { return
+	 * new Token(r2, String.valueOf(c1) + String.valueOf(c2), linenumber); }
+	 * else if (myisdigit((char) c)) { String number = buildNUM(); putback =
+	 * false; return new Token(r1, number, linenumber); } else {
+	 * System.err.println("scan: got got " + c1 + " missing " + c2 +
+	 * " (got ASCII " + c + ")"); return new Token(TK.ERROR, "bad ccase2",
+	 * linenumber); } }
+	 */
+
+	// rather than duplicating code, as done below,
+	// could use method "pointer" technique for these build methods.
+
+	// build up an ID token
+	// (could use StringBuffer to make this more efficient...)
+	private String buildID() {
+		int k = 0;
+		String str = "";
+		do {
+			str += (char) c;
+			k++;
+			c = getchar();
+		} while (myisalpha((char) c) && k < MAXLEN_ID);
+		putback = true;
+		if (myisalpha((char) c) && k == MAXLEN_ID) {
+			do {
+				c = getchar();
+			} while (myisalpha((char) c));
+			System.err.println("scan: identifier too long -- truncated to "
+					+ str);
+		}
+		return str;
 	}
-    }
 
-    // rather than duplicating code, as done below,
-    // could use method "pointer" technique for these build methods.
-
-    // build up an ID token
-    // (could use StringBuffer to make this more efficient...)
-    private String buildID() {
-        int k = 0;
-        String str = "";
-        do {
-            str += (char) c;
-            k++;
-            c = getchar();
-        } while( myisalpha((char) c) && k < MAXLEN_ID );
-        putback = true;
-        if( myisalpha((char) c) && k == MAXLEN_ID ) {
-            do { c = getchar(); } while(myisalpha((char) c));
-            System.err.println("scan: identifier too long -- truncated to "
-                               + str);
-        }
-        return str;
-    }
-
-    // build up a NUM str
-    // (could use StringBuffer to make this more efficient...)
-    private String buildNUM() {
-        int k = 0;
-        String str = "";
-        do {
-            str += (char) c;
-            k++;
-            c = getchar();
-        } while( myisdigit((char) c) && k < MAXLEN_ID );
-        putback = true;
-        if( myisdigit((char) c) && k == MAXLEN_ID ) {
-            do { c = getchar(); } while(myisdigit((char) c));
-            System.err.println("scan: number too long -- truncated to "
-                               + str);
-        }
-        return str;
-    }
-    
-    // build up an STR token
-    // (could use StringBuffer to make this more efficient...)
-    private String buildSTR() {
-        int k = 0;
-        String str = "";
-        do {
-	  if (k > 0){ str += (char) c; } 
-            k++;
-            c = getchar();
-        } while( c != '\"' && c != '\n' && c != EOF); // until right-hand DQUOTE
-	c = getchar(); // take away DQUOTE
-        putback = true;
-	if ( c != '\n' && c != ' ') {
-	    System.err.println("scan: string missing closing \"; quitting near " + str);
-	    System.exit(1);
+	// build up a NUM str
+	// (could use StringBuffer to make this more efficient...)
+	private String buildNUM() {
+		int k = 0;
+		String str = "";
+		do {
+			str += (char) c;
+			k++;
+			c = getchar();
+		} while (myisdigit((char) c) && k < MAXLEN_ID);
+		putback = true;
+		if (myisdigit((char) c) && k == MAXLEN_ID) {
+			do {
+				c = getchar();
+			} while (myisdigit((char) c));
+			System.err.println("scan: number too long -- truncated to " + str);
+		}
+		return str;
 	}
-	    // Maybe we can check if the string is missing a " if it detects a newline before the "
-//             do { c = getchar(); } while(myisalpha((char) c));
-//             System.err.println("scan: string too long -- truncated to "
-//                                + str);
-//         }
-        return str;
-    }
 
+	// build up an STR token
+	// (could use StringBuffer to make this more efficient...)
+	private String buildSTR() {
+		int k = 0;
+		String str = "";
+		do {
+			if (k > 0) {
+				str += (char) c;
+			}
+			k++;
+			c = getchar();
+		} while (c != '\"' && c != '\n' && c != EOF); // until right-hand DQUOTE
+		c = getchar(); // take away DQUOTE
+		putback = true;
+		if (c != '\n' && c != ' ') {
+			System.err
+					.println("scan: string missing closing \"; quitting near "
+							+ str);
+			System.exit(1);
+		}
+		// Maybe we can check if the string is missing a
+		// " if it detects a newline before the "
+		// do { c = getchar(); } while(myisalpha((char) c));
+		// System.err.println("scan: string too long -- truncated to "
+		// + str);
+		// }
+		return str;
+	}
 
-    // E's idea of what can form an identifier
-    // (could instead directly call Character.isLetter)
-    private static boolean myisalpha(char c) {
-        return Character.isLetter(c);
-    }
+	// E's idea of what can form an identifier
+	// (could instead directly call Character.isLetter)
+	private static boolean myisalpha(char c) {
+		return Character.isLetter(c);
+	}
 
-    // E's idea of what can form a number
-    // (could instead directly call Character.isDigit)
-    private static boolean myisdigit(char c) {
-        return Character.isDigit(c);
-    }
+	// E's idea of what can form a number
+	// (could instead directly call Character.isDigit)
+	private static boolean myisdigit(char c) {
+		return Character.isDigit(c);
+	}
 
-    // if str is a keyword, return its TK.
-    // otherwise, str is an identifier, so return TK.ID.
-    private TK keywordLookup(String str) {
-        // test for each keyword token, one after another.
-        //(not best way to handle this, but expedient.)
-        if (str.equals("var"))       return TK.VAR;
-        if (str.equals("const"))     return TK.CONST;
-        if (str.equals("print"))     return TK.PRINT;
-        if (str.equals("if"))        return TK.IF;
-        if (str.equals("then"))      return TK.THEN;
-        if (str.equals("elsif"))     return TK.ELSIF;
-        if (str.equals("else"))      return TK.ELSE;
-        if (str.equals("end"))       return TK.END;
-        if (str.equals("while"))     return TK.WHILE;
-        if (str.equals("do"))        return TK.DO;
-        if (str.equals("for"))       return TK.FOR;
-	if (str.equals("repeat"))    return TK.REPEAT; // part 12
-	if (str.equals("until"))     return TK.UNTIL; // part 12
-        if (str.equals("to"))        return TK.TO;
-        if (str.equals("downto"))    return TK.DOWNTO;
- 
-        // no keyword matched, so ...
-        return TK.ID;
-    }
+	// if str is a keyword, return its TK.
+	// otherwise, str is an identifier, so return TK.ID.
+	private TK keywordLookup(String str) {
+		// test for each keyword token, one after another.
+		// (not best way to handle this, but expedient.)
+		if (str.equals("var"))
+			return TK.VAR;
+		if (str.equals("const"))
+			return TK.CONST;
+		if (str.equals("print"))
+			return TK.PRINT;
+		if (str.equals("if"))
+			return TK.IF;
+		if (str.equals("then"))
+			return TK.THEN;
+		if (str.equals("elsif"))
+			return TK.ELSIF;
+		if (str.equals("else"))
+			return TK.ELSE;
+		if (str.equals("end"))
+			return TK.END;
+		if (str.equals("while"))
+			return TK.WHILE;
+		if (str.equals("do"))
+			return TK.DO;
+		if (str.equals("for"))
+			return TK.FOR;
+		if (str.equals("repeat"))
+			return TK.REPEAT; // part 12
+		if (str.equals("until"))
+			return TK.UNTIL; // part 12
+		if (str.equals("to"))
+			return TK.TO;
+		if (str.equals("downto"))
+			return TK.DOWNTO;
+
+		// no keyword matched, so ...
+		return TK.ID;
+	}
 
 }
